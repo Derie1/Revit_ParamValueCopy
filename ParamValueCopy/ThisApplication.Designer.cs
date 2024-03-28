df464ab952141f0f34b819ec2b9c6cecfebe9177
Mikhail Davydov <mdavydov83@gmail.com>
2024-03-28 08:25:07 +0100

    Add project files.

---
+++ ParamValueCopy/ThisApplication.Designer.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DAVY11177
+ * Date: 27.03.2024
+ * Time: 13:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace ParamValueCopy {
+
+    public sealed partial class ThisApplication : Autodesk.Revit.UI.Macros.ApplicationEntryPoint {
+        
+        public event System.EventHandler Startup;
+        
+        public event System.EventHandler Shutdown;
+        
+        /// 
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+        private void OnStartup() {
+        }
+        
+        /// 
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+        protected override void FinishInitialization() {
+            base.FinishInitialization();
+            this.OnStartup();
+            this.InternalStartup();
+            if ((this.Startup != null)) {
+                this.Startup(this, System.EventArgs.Empty);
+            }
+        }
+        
+        /// 
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+        protected override void OnShutdown() {
+            if ((this.Shutdown != null)) {
+                this.Shutdown(this, System.EventArgs.Empty);
+            }
+            base.OnShutdown();
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+        protected override string PrimaryCookie {
+            get {
+                return "ThisApplication";
+            }
+        }
+    }
+}
---
+++ ParamValueCopy/ThisApplication.cs
@@ -0,0 +1,108 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DAVY11177
+ * Date: 27.03.2024
+ * Time: 13:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xaml;
+
+
+namespace ParamValueCopy
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.DB.Macros.AddInId("6FBABC23-C6F1-4B68-815A-B32A9C6DD3C9")]
+	public partial class ThisApplication
+	{
+		private void Module_Startup(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Module_Shutdown(object sender, EventArgs e)
+		{
+
+		}
+
+		#region Revit Macros generated code
+		private void InternalStartup()
+		{
+			this.Startup += new System.EventHandler(Module_Startup);
+			this.Shutdown += new System.EventHandler(Module_Shutdown);
+		}
+        #endregion
+
+
+        /// <summary>
+        /// Method returns list with Category names stored in sorted "List<string>" variable
+        /// </summary>
+        /// <param name="doc">Gets Revit document</param>
+        /// <returns>List of category names</returns>
+        public List<string> StoreDocCategories(Document doc)
+		{
+    		Categories categories = doc.Settings.Categories;
+    		List<string> catList = new List<string>();
+    		
+    		foreach (Category cat in categories) 
+    		{
+    			if (cat.AllowsBoundParameters) { catList.Add(cat.Name); }
+    		}
+    		catList.Sort();
+    		
+    		return catList;
+		}
+		
+		/// <summary>
+		/// This method just translate List of strings into a multiline one-string text to put it into a MessageBox or logfile.
+		/// </summary>
+		/// <param name="inputList">List of strings</param>
+		/// <returns>One-string element formatted in multiline text</returns>
+		public string OutputFormatter(List<string> inputList)
+		{
+			string result = "";
+			//string lineTab = "    ";
+			
+			foreach (var elem in inputList)
+			{
+				result += elem + "\n";
+			}
+			return result;
+		}
+		
+		/// <summary>
+		/// This method display MessageBox with list of all document categories.
+		/// </summary>
+		public void ShowCategories()
+		{
+			//List<string> catList = new List<string> {"string1", "string2", "string3"};
+			UIDocument uidoc = this.ActiveUIDocument;
+			Document doc = uidoc.Document;
+			List<string> categoryList = StoreDocCategories(doc);
+						
+			TaskDialog.Show("All categories", "List of categories in project: \n\n" + OutputFormatter(categoryList));
+		}
+		
+		public void ShowWindow()
+		{
+			MainWindow window1 = new MainWindow();
+			window1.Show();
+		}
+	}
+}
